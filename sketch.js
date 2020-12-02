6c778eae4ea976d2097cb12674561069f18b5e21
DishitaLumba <69897366+DishitaLumba@users.noreply.github.com>
2020-12-02 18:01:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,125 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+
+var score=0;
+
+function preload(){
+  polygon_img=loadImage("polygon.png");
+  
+}
+function setup() {
+  createCanvas(900,400);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+
+  ground = new Ground(450,380,900,10);
+  stand1 = new Stand(390,300,250,10);
+  stand2 = new Stand(700,200,200,10);
+ 
+  //level one
+  Box1 = new Box(300,275,30,40);
+  console.log(Box1);
+  Box2 = new Box(330,275,30,40);
+  Box3 = new Box(360,275,30,40);
+  Box4 = new Box(390,275,30,40);
+  Box5 = new Box(420,275,30,40);
+  Box6 = new Box(450,275,30,40);
+  Box7 = new Box(480,275,30,40);
+  //level two
+  Box8 = new Box(330,235,30,40);
+  Box9 = new Box(360,235,30,40);
+  Box10 = new Box(390,235,30,40);
+  Box11 = new Box(420,235,30,40);
+  Box12 = new Box(450,235,30,40);
+  //level three
+  Box13 = new Box(360,195,30,40);
+  Box14 = new Box(390,195,30,40);
+  Box15 = new Box(420,195,30,40);
+  //top
+  Box16 = new Box(390,155,30,40);
+
+  //set 2 for second stand
+  //level one
+  Boxs1 = new Box(640,175,30,40);
+  Boxs2 = new Box(670,175,30,40);
+  Boxs3 = new Box(700,175,30,40);
+  Boxs4 = new Box(730,175,30,40);
+  Boxs5 = new Box(760,175,30,40);
+  //level two
+  Boxs6 = new Box(670,135,30,40);
+  Boxs7 = new Box(700,135,30,40);
+  Boxs8 = new Box(730,135,30,40);
+  //top
+  Boxs9 = new Box(700,95,30,40);
+
+  //polygon holder with slings
+  polygon = Bodies.circle(50,200,20);
+  World.add(world,polygon);
+  
+  slingShot = new Slingshot(polygon,{x:100,y:200});
+
+}
+function draw() {
+  background("lightbrown")
+  ground.display();
+  stand1.display();
+  stand2.display();
+  strokeWeight(2);
+  stroke(15);
+  Box1.display();
+  Box2.display();
+  Box3.display();
+  Box4.display();
+  Box5.display();
+  Box6.display();
+  Box7.display();
+  Box8.display();
+  Box9.display();
+  Box10.display();
+  Box11.display();
+  Box12.display();
+  Box13.display();
+  Box14.display();
+  Box15.display();
+  Box16.display();
+  Boxs1.display();
+  Boxs2.display();
+  Boxs3.display();
+  Boxs4.display();
+  Boxs5.display();
+  Boxs6.display();
+  Boxs7.display();
+  Boxs8.display();
+  Boxs9.display();
+  
+  imageMode(CENTER)
+  image(polygon_img ,polygon.position.x,polygon.position.y,40,40);
+
+  slingShot.display();
+  
+}
+function mouseDragged(){
+  Matter.Body.setPosition(polygon,{x:mouseX,y:mouseY});
+}
+function mouseReleased(){
+  slingShot.fly();
+}
+function keyPressed(){
+  if(keyCode === 32){
+      slingShot.attach(polygon);
+    
+  }
+}
+
+
+
+
+
+
+
+
+
